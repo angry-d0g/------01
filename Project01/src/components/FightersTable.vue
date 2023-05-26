288e897ec9bf1577ef6262d81373ff9ab64a9cc7
Xopek <alexxxhrn@gmail.com>
2023-05-26 12:58:09 +0300

    a

---
+++ Project01/src/components/FightersTable.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  props: {
+    items: Array
+  },
+}
+</script>
+
+<template>
+  <table id="fighter-table" >
+    <tr>
+        <th>Rank</th>
+        <th>Name</th>
+        <th>Country</th>
+        <th>Number of Fights</th>
+        <th>Age</th>
+        <th>Elo Ranking</th>
+    </tr>
+    <tr v-for="(row, index) in items">
+        <td>{{ index + 1 }}</td>
+        <td>{{ row.name }}</td>
+        <td>{{ row.country}}</td>
+        <td>{{ row.fightsNum }}</td>
+        <td>{{ row.age }}</td>
+        <td>{{ row.elo }}</td>
+    </tr>
+    
+    <!-- Add more rows for other fighters and their rankings -->
+</table>
+</template>
---
+++ Project01/src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<script>
+import FightersTable from '../components/FightersTable.vue';
+
+export default {
+  components: { FightersTable },
+  data() {
+    return {
+      fighters: [
+        {
+          id: 1,
+          name: 'Konnor',
+          age: '34',
+          country: 'Ireland',
+          fightsNum: 56,
+          elo: 1583,
+          fightStory: [
+            {
+              opponent: { id: 2, name: 'Khabib' },
+              result: 'win',
+              reason: 'tko'
+            },
+            {
+              opponent: { id: 2, name: 'Khabib' },
+              result: 'win',
+              reason: 'tko'
+            }
+          ],
+        },
+
+        {
+          id: 3,
+          name: 'Konnor',
+          age: '34',
+          country: 'Irland',
+          fightsNum: 56,
+          elo: 1583,
+          fightStory: [
+            {
+              opponent: { id: 2, name: 'Khabib' },
+              result: 'win',
+              reason: 'tko'
+            },
+            {
+              opponent: { id: 2, name: 'Khabib' },
+              result: 'win',
+              reason: 'tko'
+            }
+          ],
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <FightersTable :items="fighters">
+  </FightersTable>
+</template>
